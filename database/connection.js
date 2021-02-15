6b52ae37865f8b26f10089fd586a8c25616d5460
Newt <dspitzli9@gmail.com>
2021-02-15 09:14:02 +0100

    structure Update

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const mysql = require('mysql');
+
+const pool  = mysql.createPool({
+  connectionLimit : 10,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PW,
+  database: process.env.DATABASE_DB
+});
+
+module.exports = pool;
--- router.js
+++ router.js
@@ -3,5 +3,8 @@ module.exports = (app) => {
     app.use('/', require('./routes/index'));
 
     // '/authorize'
+    app.use('/api', require('./routes/api'));
+
+    // '/authorize'
     app.use('/authorize', require('./routes/discord'));
 }
