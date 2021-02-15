62c01671da01b946539d46da8f98be47939fc855
Newt <dspitzli9@gmail.com>
2021-02-15 09:27:03 +0100

    Bot Update

--- router.js
+++ router.js
@@ -2,9 +2,11 @@ module.exports = (app) => {
     // '/'
     app.use('/', require('./routes/index'));
 
-    // '/authorize'
+    // '/api'
     app.use('/api', require('./routes/api'));
 
     // '/authorize'
     app.use('/authorize', require('./routes/discord'));
+
+    app.use('/bot', require('./discord/bot'))
 }
