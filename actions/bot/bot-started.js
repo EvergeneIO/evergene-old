1247c6451444c1f921caa5cba82255c941eaf023
Newt <dspitzli9@gmail.com>
2021-02-24 13:43:41 +0100

    Actions for DC logs

---
+++ actions/bot/bot-started.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+const con = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
+});
+const fs = require('fs');
+
+const start = fs.readFileSync('start.txt');
+
+const embed = {
+    embeds: [{
+        title: `System Online`,
+        color: 65280,
+        footer: {
+            text: `took ${Date.now() - start}ms`
+          }
+    }]
+};
+
+fetch(`https://discord.com/api/v8/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}/messages/813418805736505375`, {
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(embed),
+})
+    .then(res => res.json())
+    .then(json => console.log(json));
