2e7e6f7911eee0fc6c4c6f38f73ca1b256bb2596
NewtTheWolf <dominik@spitzli.dev>
2021-03-26 16:28:30 +0100

    create test.js

---
+++ routes/web/test.js
@@ -0,0 +1,12 @@
+const Endpoint = require('../../classes/MainEndpoint');
+
+module.exports = (router, filename, path) => {
+
+    new Endpoint(router, filename, {
+        method: Endpoint.GET,
+        path
+    }, null,
+        async (req, res, filename, lang, version, title, user) => {
+            res.render(filename, { version: version, pageTitle: title, user: user });
+        })
+};
