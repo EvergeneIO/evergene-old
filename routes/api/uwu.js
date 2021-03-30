3aeddf52880cfd3c7cf029a7b7685d9ae0ed14a9
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-03-30 17:54:03 +0200

    update

--- routes/api/uwu.js
+++ routes/api/uwu.js
@@ -5,11 +5,9 @@ module.exports = (server, filename, path) => {
     new Endpoint(server, filename, {
         method: Endpoint.GET,
         path
-    }, null,
+    }, 8,
         async (req, res, endpoint, tools) => {
-            console.log(req.params.test)
-            let output = await tools.image(endpoint);
             res.header("Content-Type", "application/json");
-            res.send(JSON.stringify({ url: output }, null, 3));
+            res.send(JSON.stringify({ url: 'output' }, null, 3));
         });
 }
