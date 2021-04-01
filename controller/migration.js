08952c8836d505c39ad1be781e6aceb31045d99b
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-04-01 22:16:04 +0200

    UP

--- controller/migration.js
+++ controller/migration.js
@@ -1,16 +1,48 @@
+const chalk = require("chalk");
 const fs = require("fs");
 const fetch = require("node-fetch");
-const router = require('express').Router();
-let filename = "test.sql"
+const con = require("../database/connection");
 
- let stream = fs.createWriteStream(`./migration/${filename}`);
+module.exports = () => {
+    return new Promise(async resolve => {
+        let files = fs.readdirSync("../migration/");
+        resolve();
+        try {
+        con.query("SELECT timestamp FROM migrations", async (err, res) => {
+            try {
+            if (err) {
+                
+                // ! Create Table Migration (User etc) + Timestamp Entry in 'migrations' (Use the Newest "create" migration)
+                await new Promise((resolve1, reject1) => {
+                    con.query("CREATE TABLE migrations (timestamp VARCHAR(255))", (err1, res1) => {
+                        if (!err) reject1(err);
+                        resolve1();
+                    });
+                })
+            } else {
+               files.filter()
+            }
+            resolve();
+        } catch(e) {
+            throw e;
+        }
+        })
+        } catch(e) {
+             console.log(chalk.redBright("[MIGRATION] There was an error during the migraton! Please contact the Evergene support."))
+             console.error(e);
+        }
+    })
+}
 
-fetch(`http://localhost:3001/${filename}`).then((binary) => {
-    binary.body.pipe(stream);
-});
-stream.on("finish", () => console.log("Finish!"))
-module.exports = router;
+// 1
+// 2
+// 3
+// 4
+//? 2 = 3 + 4
+//CREATE TABLE migrations (timestamp VARCHAR(255))
 
-
-//AUFBAU
-// TABLES.json > all file names etc.
+/**
+ * on start: check if migration table Exist?
+ * yes = check latest Migration
+ * no = create Table
+ */
---
+++ temp.js
@@ -0,0 +1,7 @@
+/**
+ * /u/newt
+ * /u/newt/views = /views/newt
+ * /u/newt/static = /static
+ * /u/newt/web = /routes/web/newt
+ * 
+ */
